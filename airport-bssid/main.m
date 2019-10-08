431ab0b18ed0c7cf7753434600a113bb6b1448ba
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-10-08 12:37:57 +0300

    Support two commands: scan, connect
    Also show Wi-Fi band in scan results

--- airport-bssid/main.m
+++ airport-bssid/main.m
@@ -10,7 +10,7 @@
 #import <CoreWLAN/CoreWLAN.h>
 
 NSString *usage_string(NSString *arg0) {
-    return [NSString stringWithFormat:@"usage: %@ <ifname> [<bssid>] [<password>]", arg0];
+    return [NSString stringWithFormat:@"usage: %@ <ifname> <command>\n\nCommands:\n\tconnect [bssid] [password]\n\tscan [name] [band:2.4|5]", arg0];
 }
 
 void dump_error(NSString *message) {
@@ -29,6 +29,14 @@ char const *phyModeName( enum CWPHYMode n )
         case kCWPHYMode11b: return "802.11b";
         default: return "other/unknown";
             
+    }
+}
+
+char const *bandName(enum CWChannelBand n) {
+    switch ((int)n) {
+        case kCWChannelBand2GHz: return "2.4";
+        case kCWChannelBand5GHz: return "5";
+        default: return "N/I";
     }
 }
 
@@ -53,16 +61,31 @@ int main(int argc, const char * argv[])
          */
         
         // args check and initialization
-        if (!(argc > 1)) dump_error(usage_string([NSString stringWithUTF8String:argv[0]]));
+        if (argc < 3) dump_error(usage_string([NSString stringWithUTF8String:argv[0]]));
         NSString *interfaceName = [NSString stringWithUTF8String:argv[1]];
+        NSString *command = [NSString stringWithUTF8String:argv[2]];
+        
         NSString *bssid = nil;
-        if (argc > 2) {
-            bssid = [NSString stringWithUTF8String:argv[2]];
-        }
         NSString *password = nil;
-        if (argc > 3) {
-            password = [NSString stringWithUTF8String:argv[3]];
-        }
+        
+        NSString *ssidName = nil;
+        NSString *band = nil;
+    
+        if ([command isEqualToString:@"scan"]) {
+            if (argc > 3) {
+                ssidName = [NSString stringWithUTF8String:argv[3]];
+            }
+            if (argc > 4) {
+                band = [NSString stringWithUTF8String:argv[4]];
+            }
+        } else if ([command isEqualToString:@"connect"]) {
+            if (argc > 3) {
+                bssid = [NSString stringWithUTF8String:argv[3]];
+            }
+            if (argc > 4) {
+                password = [NSString stringWithUTF8String:argv[4]];
+            }
+        } else dump_error(usage_string([NSString stringWithUTF8String:argv[0]]));
         
         // interface check
         CWInterface *interface = [[[CWWiFiClient alloc] init] interfaceWithName:interfaceName];
@@ -70,29 +93,33 @@ int main(int argc, const char * argv[])
             dump_error(@"The interface is down. Please activate the interface before connecting to network!");
         
         printf("Notice: The interface %s is in %s phyMode.\n", [interfaceName cStringUsingEncoding:NSUTF8StringEncoding], phyModeName(interface.activePHYMode));
-        
+
         // search for target bssid
         NSError *error = nil;
         NSSortDescriptor *nameDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"ssid" ascending:YES];
-        NSArray *scan = [[interface scanForNetworksWithSSID:nil error:&error] sortedArrayUsingDescriptors:[NSArray arrayWithObject:nameDescriptor]];
+        NSArray *scan = [[interface scanForNetworksWithName:ssidName includeHidden:true error:&error] sortedArrayUsingDescriptors:[NSArray arrayWithObject:nameDescriptor]];
         if (error)
             dump_error([NSString stringWithFormat:@"An error has been occurred while scanning networks: %@", error]);
         CWNetwork *targetNetwork = nil;
         
         printf("\x1B[0m***** Scanned networks *****\n");
-        printf("%24s, %17s, %3s, RSSI(dBm)\n", "ESSID", "BSSID", "Ch");
+        printf("%24s, %17s, %4s, %3s, RSSI(dBm)\n", "ESSID", "BSSID", "Band", "Ch");
         for (CWNetwork *network in scan) {
+            const char * bn = bandName(network.wlanChannel.channelBand);
+            if (band != nil) {
+                if (![band isEqualToString:[NSString stringWithUTF8String:bn]]) continue;
+            }
             if ([network.bssid isEqualToString:bssid]) {
                 targetNetwork = network;
-                printf("%s%24s, %17s, %3lu, %3ld\n", "\x1B[32m", [network.ssid cStringUsingEncoding:NSUTF8StringEncoding], [network.bssid cStringUsingEncoding:NSUTF8StringEncoding], (unsigned long)network.wlanChannel.channelNumber, (long)network.rssiValue);
+                printf("%s%24s, %17s, %4s, %3lu, %3ld\n", "\x1B[32m", [network.ssid cStringUsingEncoding:NSUTF8StringEncoding], [network.bssid cStringUsingEncoding:NSUTF8StringEncoding], bn, (unsigned long)network.wlanChannel.channelNumber, (long)network.rssiValue);
             } else {
-                printf("%s%24s, %17s, %3lu, %3ld\n", "\x1B[0m", [network.ssid cStringUsingEncoding:NSUTF8StringEncoding], [network.bssid cStringUsingEncoding:NSUTF8StringEncoding], (unsigned long)network.wlanChannel.channelNumber, (long)network.rssiValue);
+                printf("%s%24s, %17s, %4s, %3lu, %3ld\n", "\x1B[0m", [network.ssid cStringUsingEncoding:NSUTF8StringEncoding], [network.bssid cStringUsingEncoding:NSUTF8StringEncoding], bn, (unsigned long)network.wlanChannel.channelNumber, (long)network.rssiValue);
             }
         }
         printf("\x1B[0m****************************\n");
 
         if (bssid == nil) {
-            dump_error([NSString stringWithFormat:@"Network scan completed. If you want to connect to specific BSSID, please enter the command below:\n %@ <ifname> <bssid> [<password>]", [NSString stringWithUTF8String:argv[0]]]);
+            exit(0);
         }
 
         if (targetNetwork == nil)
